0acdab40264f3e572be12c8e23296c3b0680580f
Deividas Rimkus <8516648+Lisviks@users.noreply.github.com>
2022-09-08 05:51:46 +0100

    mobile design

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
 const featuresBtn = document.querySelector('.features');
 const companyBtn = document.querySelector('.company');
+const menuBtn = document.querySelector('.menu-icon');
+const nav = document.querySelector('nav');
 
-featuresBtn.addEventListener('click', () => {
-  featuresBtn.classList.toggle('open');
+featuresBtn.addEventListener('click', (e) => {
+  const classlist = e.target.classList;
+  if (classlist.contains('open-close-list') || classlist.contains('arrow')) {
+    featuresBtn.classList.toggle('open');
+  }
 });
 
-companyBtn.addEventListener('click', () => {
-  companyBtn.classList.toggle('open');
+companyBtn.addEventListener('click', (e) => {
+  const classlist = e.target.classList;
+  if (classlist.contains('open-close-list') || classlist.contains('arrow')) {
+    companyBtn.classList.toggle('open');
+  }
+});
+
+menuBtn.addEventListener('click', () => {
+  menuBtn.classList.toggle('open');
 });
